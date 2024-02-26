a07769b2e1298dcf1cc7d02024036486bb79c57e
MaksGavriliuk <maks.revival27@gmail.com>
2024-02-27 01:44:43 +0300

    refactor: change docker port

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,14 +1,15 @@
+CREATE TABLE subjects
+(
+    id           BIGSERIAL PRIMARY KEY,
+    subject_name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE assessments
 (
-    id         BIGSERIAL INT PRIMARY KEY,
+    id         BIGSERIAL PRIMARY KEY,
     student_id INT NOT NULL,
     subject_id INT NOT NULL,
     score      INT NOT NULL,
     FOREIGN KEY (subject_id) REFERENCES subjects (id)
 );
 
-CREATE TABLE subjects
-(
-    id           BIGSERIAL INT PRIMARY KEY,
-    subject_name VARCHAR(255) NOT NULL
-);
